aef6f4b8a469e25bc38d72fbeba1cb637d09a97c
Michée Kolony <micheekolony71@gmail.com>
2026-07-06 14:22:04 +0100

    ajout modale modif

--- src/app/composants/detailarticle/detailarticle.component.ts
+++ src/app/composants/detailarticle/detailarticle.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -12,10 +12,26 @@ export class DetailarticleComponent implements OnInit {
   private articleUrl = "https://backend-kinova.onrender.com/article/";
 
   article: any = null;
-
   imagePrincipale = '';
-
   loading = true;
+
+  // MODAL
+  showModal = false;
+  loadingUpdate = false;
+
+  // FORM DATA
+  formData: any = {
+    nom: '',
+    prix: '',
+    reduction: '',
+    categorie: '',
+    genre: '',
+    description: '',
+    vendeurNom: '',
+    vendeurTelephone: ''
+  };
+
+  newImages: File[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -23,7 +39,10 @@ export class DetailarticleComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.loadArticle();
+  }
 
+  loadArticle() {
     const id = this.route.snapshot.paramMap.get('id');
 
     if (id) {
@@ -39,11 +58,61 @@ export class DetailarticleComponent implements OnInit {
         }
       });
     }
-
   }
 
   changerImage(image: string) {
     this.imagePrincipale = image;
   }
 
+  openModal() {
+    this.formData = {
+      nom: this.article.nom,
+      prix: this.article.prix,
+      reduction: this.article.reduction,
+      categorie: this.article.categorie,
+      genre: this.article.genre,
+      description: this.article.description,
+      vendeurNom: this.article.vendeurNom,
+      vendeurTelephone: this.article.vendeurTelephone
+    };
+
+    this.showModal = true;
+  }
+
+  closeModal() {
+    this.showModal = false;
+    this.newImages = [];
+  }
+
+  onFileChange(event: any) {
+    this.newImages = Array.from(event.target.files);
+  }
+
+  updateArticle() {
+    this.loadingUpdate = true;
+
+    const id = this.article._id;
+
+    const formData = new FormData();
+
+    Object.keys(this.formData).forEach(key => {
+      formData.append(key, this.formData[key]);
+    });
+
+    this.newImages.forEach(file => {
+      formData.append('images', file);
+    });
+
+    this.http.put(this.articleUrl + id, formData).subscribe({
+      next: (res: any) => {
+        this.loadingUpdate = false;
+        this.showModal = false;
+        this.loadArticle();
+      },
+      error: (err) => {
+        console.log(err);
+        this.loadingUpdate = false;
+      }
+    });
+  }
 }
