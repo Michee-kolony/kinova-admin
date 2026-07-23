25cbdc5f12f1fdd745ab35fd10501eebdeceed77
Michée Kolony <micheekolony71@gmail.com>
2026-07-23 15:14:21 +0100

    mises à jour

--- src/app/client/footer/footer.component.ts
+++ src/app/client/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AppRoutingModule } from "../../app-routing.module";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
 
