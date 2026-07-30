fd6325ad5b92dab9377e86338d7c8ea712c190cf
Michée Kolony <micheekolony71@gmail.com>
2026-07-30 23:40:47 +0100

    ajout des amélioration

--- src/app/client/kinova/kinova.component.ts
+++ src/app/client/kinova/kinova.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-kinova',
   templateUrl: './kinova.component.html',
-  styleUrl: './kinova.component.css'
+  styleUrls: ['./kinova.component.css']
 })
-export class KinovaComponent {
+export class KinovaComponent implements OnInit {
+
+  showScrollButton = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+
+    // Remonter automatiquement en haut à chaque changement de page
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe(() => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      });
+
+  }
+
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.showScrollButton = window.pageYOffset > 150;
+  }
+
+
+  scrollToTop(): void {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
 
 }
